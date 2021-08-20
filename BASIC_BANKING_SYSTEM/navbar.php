d3f8286862d787b2bb348482e1a583bfdaee4e75
Yogesh s chowdgund <yogeshsc2000@gmail.com>
2021-08-20 11:29:13 +0530

    added basic banking system

---
+++ BASIC_BANKING_SYSTEM/navbar.php
@@ -0,0 +1,37 @@
+<!-- navbar --> 
+
+<nav class="navbar navbar-expand-md navbar-light bg-light" >
+
+<a class="navbar-brand" href="index.php" style="color: purple;">Bank Of Karnataka</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+
+  <span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item">
+
+          <a class="nav-link" href="index.php" style="color: purple;">HOME</a>
+
+        </li>
+
+        <li class="nav-item">
+
+          <a class="nav-link" href="transfermoney.php" style="color: purple;">VIEW CUSTOMERS</a>
+
+        </li>
+
+        <li class="nav-item">
+
+          <a class="nav-link" href="transactionhistory.php" style="color: purple;">HISTORY</a>
+
+        </li>
+
+</div>
+
+</nav>
